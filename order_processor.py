35ea42d4851fed365576ebf5b06afb26a34d8272
Josie Kiew <josiekiew@MacBook-Pro.local>
2026-08-20 11:02:05 +0800

    changed ordering of assignee

--- order_processor.py
+++ order_processor.py
@@ -209,6 +209,7 @@ def process_orders():
         processed_rows.append({
             "Shopify ID": shopify_id,
             "Completed": completed,
+            "Assignee": assignee,
             "Order": order_id,
             "SKU": main_sku,
             "Custom Details": custom_details,
@@ -219,8 +220,8 @@ def process_orders():
             "Scent": scent,
             "Delivery Slot": delivery_slot,
             "Delivery Date": delivery_date,
-            "Delivery Type": delivery_type,
-            "Assignee": assignee
+            "Delivery Type": delivery_type
+           
             
         })
 
